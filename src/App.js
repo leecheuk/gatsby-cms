31ca0299f52271c7b193a8e0e194d1a132588233
m <dev.cheuk.lee@gmail.com>
2020-01-20 01:29:26 -0500

    centralized scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import RouterApp from './router';
+import './assets/scss/stylesheet.scss';
 
 function App() {
   return (
