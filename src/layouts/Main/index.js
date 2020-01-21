0a0a904b7e062745ef3756cdddbd8db30aa83fce
m <dev.cheuk.lee@gmail.com>
2020-01-21 00:56:55 -0500

    added props

--- src/layouts/Main/index.js
+++ src/layouts/Main/index.js
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types';
 import styles from './main.module.scss';
 import Title from '../components/Title';
 
-const Main = ({children}) => {
+const Main = ({children, title, subtitle}) => {
     return (
         <div className={styles["layout__main"]}>
-            <Title/>
+            <Title title={title} subtitle={subtitle}/>
             {children}
         </div>
     );
 };
 
 Main.propTypes = {
-    children: PropTypes.node
+    children: PropTypes.node,
+    title: PropTypes.string,
+    subtitle: PropTypes.string
 }
 
 export default Main;
