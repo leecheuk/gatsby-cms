be5cec3638e5faf81c7395ea26c2b29aa1ba2ba6
m <dev.cheuk.lee@gmail.com>
2020-01-21 01:44:31 -0500

    hide menu bar

--- public/electron.js
+++ public/electron.js
@@ -16,6 +16,7 @@ function createWindow() {
             nodeIntegration: true
         }
     });
+    mainWindow.removeMenu();
     // load react app
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : 
         `file://${path.join(__dirname, '../build/index.html')}`);
