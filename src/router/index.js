f98c8637c193c133ad0dc86b23d01f0a6f28dca2
m <dev.cheuk.lee@gmail.com>
2020-01-21 01:02:49 -0500

    updated profile view name

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
-import Page1 from '../views/Page1';
+import Profile from '../views/Profile';
 import Page2 from '../views/Page2';
 
 const router = () => (
         <Switch>
             <Route exact path="/">
-                <Page1 />
+                <Profile />
             </Route>
             <Route exact path="/page2">
                 <Page2 />
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import LayoutMain from '../layouts/Main';
 
 const Page1 = () => {
     return (
-        <div>
-            Page 1
-            <Link to="/page2">page2</Link>
-        </div>
+        <LayoutMain title="Profile">
+            Profile
+        </LayoutMain>
     );
 };
 
