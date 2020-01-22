32369d38a697f65265c34537b38766f819441eca
m <dev.cheuk.lee@gmail.com>
2020-01-21 22:31:18 -0500

    improve link clicking ux

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,11 +3,12 @@ import {Link} from 'react-router-dom';
 import styles from './sidebar.module.scss';
 
 const Sidebar = () => {
+    
     return (
         <div className={styles['sidebar']}>
             <ul className={styles['sidebar__list']}>
-                <li className={styles['sidebar__item']}><Link to="/profile">Profile</Link></li>
-                <li className={styles['sidebar__item']}><Link to="/">Dashboard</Link></li>
+                <Link to="/profile"><li className={styles['sidebar__item']}>Profile</li></Link>
+                <Link to="/"><li className={styles['sidebar__item']}>Dashboard</li></Link>
                 <li className={styles['sidebar__item']}>Posts</li>
                 <li className={styles['sidebar__item']}>Skills</li>
                 <li className={styles['sidebar__item']}>Portfolio</li>
