0bb005e0c0891da0c2a246ab14980116145bbc6f
m <dev.cheuk.lee@gmail.com>
2020-01-21 21:11:03 -0500

    ignore dev server from watching gatsby file changes

--- .webpack.config.js
+++ .webpack.config.js
@@ -1,4 +1,13 @@
+const path = require('path');
+const {appendWebpackPlugin} = require('@rescripts/utilities');
+const webpack = require('webpack');
+
 module.exports = config => {
     config.target = 'electron-renderer';
+    
+    return appendWebpackPlugin(
+        new webpack.WatchIgnorePlugin(['./public/website/']),
+        config
+    )
     return config;
 }
