77ce6f1989acfe88c33e7833ce55019a37a98cff
m <dev.cheuk.lee@gmail.com>
2020-01-21 23:12:06 -0500

    Added active link css

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import styles from './sidebar.module.scss';
 
 const Sidebar = () => {
-    
+    let activeStyle = {
+        background: '#41454c',
+        color: '#FFF'
+    }
     return (
         <div className={styles['sidebar']}>
             <ul className={styles['sidebar__list']}>
-                <Link to="/profile"><li className={styles['sidebar__item']}>Profile</li></Link>
-                <Link to="/"><li className={styles['sidebar__item']}>Dashboard</li></Link>
+                <NavLink activeClassName={styles['sidebar__link-active']} exact to="/profile"><li className={styles['sidebar__item']}>Profile</li></NavLink>
+                <NavLink activeClassName={styles['sidebar__link-active']} exact to="/"><li className={styles['sidebar__item']}>Dashboard</li></NavLink>
                 <li className={styles['sidebar__item']}>Posts</li>
                 <li className={styles['sidebar__item']}>Skills</li>
                 <li className={styles['sidebar__item']}>Portfolio</li>
