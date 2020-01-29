a2fa53ef2e453fb7bcd4b02707f7f525fa20a4ee
m <dev.cheuk.lee@gmail.com>
2020-01-28 23:06:26 -0500

    added routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,19 @@ import {Switch, Route} from 'react-router-dom';
 
 import Profile from '../views/Profile';
 import Dashboard from '../views/Dashboard';
+import Posts from '../views/Posts';
 
 const router = () => (
         <Switch>
             <Route exact path="/">
                 <Dashboard />
-        </Route>
-        <Route exact path="/profile">
-            <Profile />
-        </Route>
+            </Route>
+            <Route exact path="/profile">
+                <Profile />
+            </Route>
+            <Route exact path="/posts">
+                <Posts />
+            </Route>
         </Switch>
 );
 
